ffc1acd87296b9cc5ad311de08043ebf074d9d32
Utpal Kumar <utpalkumar567@gmail.com>
2024-03-25 22:41:37 +0530

    mobile friendly custmization

--- src/Component/Head.js
+++ src/Component/Head.js
@@ -3,9 +3,11 @@ import React from "react";
 const Head = () => {
   return (
     <div className="flex place-content-between py-6 shadow-lg">
-      <span className=" pl-12 font-mono text-2xl font-extrabold">
-        Utpal.Dev
-      </span>
+      <a href="/">
+        <span className=" pl-12 font-mono text-2xl font-extrabold cursor-pointer">
+          Utpal.Dev
+        </span>
+      </a>
       <ul className="flex pr-12">
         <li className=" px-4 text-lg font-bold">Home</li>
         <li className=" px-4 text-lg font-bold">About</li>
